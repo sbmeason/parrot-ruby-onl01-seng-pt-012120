c1c426a3dc5776e262d480f2d4157d0741cfef3f
Samantha Meason <sbmeason@gmail.com>
2020-01-26 18:29:12 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,24 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot
+  puts "Squawk!"
+  "Squawk!"
+end
+def parrot
+  puts "Pretty bird!"
+  "Pretty bird!"
+end
+def parrot
+  puts "Squawk!"
+  "Squawk!"
+end
+
+def parrot(sound = "Squawk!")
+  puts #{sound}
+  "Squawk"
+end
+sound("Pretty bird!")
+
+
+
+  
